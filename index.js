7911a96b6cb7502bee52e2c4ddac6ac8120a5bf2
SamDump <samokoder@yandex.ru>
2020-05-21 19:22:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(msg) {
 function logWhisper(msg) {
   console.log(whisper(msg));
 }
+
+function sayHiToGrandma(string) {
+  if (string === whisper(string)) {
+    return "I can't hear you!";
+  } else if (string === shout(string)) {
+    return 'YES INDEED!';
+  } else if (string === 'I love you, Grandma.') {
+    return 'I love you, too.';
+  }
+}
